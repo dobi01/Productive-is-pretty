13b6e8866ee279addca46672c7fb586951b626e0
Dobi Okrasa <33764931+dobi01@users.noreply.github.com>
2018-08-14 22:53:10 +0200

    Fix code formatting on GitHub in Column.js

--- scripts/Column.js
+++ scripts/Column.js
@@ -1,54 +1,54 @@
 function Column(id, name) {
   
   var self = this;
-	this.id = id;
-	this.name = name || 'No name given';
-	this.element = createColumn();
+  this.id = id;
+  this.name = name || 'No name given';
+  this.element = createColumn();
 
-	function createColumn() {
-		var column = $('<div class="column"></div>'),
-		    columnTitle = $('<h2 class="column-title">' + self.name + '</h2>'),
-		    columnCardList = $('<ul class="card-list"></ul>'),
-		    columnDelete = $('<button type="button" class="btn-delete">x</button>'),
-		    columnAddCard = $('<button type="button" class="column-add-card">Dodaj kartę</button>');
+  function createColumn() {
+    var column = $('<div class="column"></div>'),
+        columnTitle = $('<h2 class="column-title">' + self.name + '</h2>'),
+        columnCardList = $('<ul class="card-list"></ul>'),
+        columnDelete = $('<button type="button" class="btn-delete">x</button>'),
+        columnAddCard = $('<button type="button" class="column-add-card">Dodaj kartę</button>');
 
-		columnDelete.click(function() {
-			self.deleteColumn();
-		});
+    columnDelete.click(function() {
+      self.deleteColumn();
+    });
 
     columnTitle.click(function() {
       self.updateColumnTitle();
     });
 
-		columnAddCard.click(function() {
+    columnAddCard.click(function() {
       var cardName = prompt("Enter the name of the card");
 
-			$.ajax({
-		    url: baseUrl + '/card',
-		    method: 'POST',
-		    data: {
+      $.ajax({
+        url: baseUrl + '/card',
+        method: 'POST',
+        data: {
           name: cardName,
           bootcamp_kanban_column_id: self.id
-		    },
-		    success: function(response) {
-	        var card = new Card(response.id, self.id, cardName);
-	        self.createCard(card);
-			  }
-			});
+        },
+        success: function(response) {
+          var card = new Card(response.id, self.id, cardName);
+          self.createCard(card);
+        }
+      });
     });
-    
-		column.append(columnTitle)
+
+    column.append(columnTitle)
           .append(columnDelete)
           .append(columnAddCard)
           .append(columnCardList);
-      
-		return column;
+
+    return column;
   }
 }
 
 Column.prototype = {
-	createCard: function(card) {
-	  this.element.children('ul').append(card.element);
+  createCard: function(card) {
+    this.element.children('ul').append(card.element);
   },
 
   updateColumnTitle: function() {
@@ -67,8 +67,9 @@ Column.prototype = {
     });
   },
 
-	deleteColumn: function() {
+  deleteColumn: function() {
     var self = this;
+	  
     $.ajax({
       url: baseUrl + '/column/' + self.id,
       method: 'DELETE',
