d96f4ccd1ec463764cb5627f33cfda5ada4586f6
Dobi Okrasa <33764931+dobi01@users.noreply.github.com>
2018-08-14 22:44:28 +0200

    Fix code formatting on GitHub in Board.js

--- scripts/Board.js
+++ scripts/Board.js
@@ -6,26 +6,27 @@ function initSortable() {
 }
 
 var board = {
-	name: 'MyKanbanBoard',
-	createColumn: function(column) {
-	  this.element.append(column.element);
-	  initSortable();
-	},
-	element: $('#board .column-container')
+  name: 'MyKanbanBoard',
+  createColumn: function(column) {
+    this.element.append(column.element);
+    initSortable();
+  },
+  element: $('#board .column-container')
 };
 
 $('.create-column')
   .click(function() {
     var columnName = prompt('Enter a column name');
+	
     $.ajax({
-		url: baseUrl + '/column',
-		method: 'POST',
-		data: {
-    	name: columnName
-		},
-		success: function(response){
-			var column = new Column(response.id, columnName);
-			board.createColumn(column);
-      	}
-    });
+      url: baseUrl + '/column',
+      method: 'POST',
+      data: {
+       name: columnName
+      },
+      success: function(response) {
+        var column = new Column(response.id, columnName);
+        board.createColumn(column);
+      }
+  });
 });
